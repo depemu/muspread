8a0c56e0b623c2a1f1b7809cfbe45b7a26c1baf4
Muhar Dwi Purwanto <k@muhar.us>
2019-12-01 17:09:23 +0700

    ADD: try catch on read method

--- private/libraries/Muspread.php
+++ private/libraries/Muspread.php
@@ -10,13 +10,27 @@ class Muspread {
     $this->_service = new Google_Service_Sheets($client);
   }
 
+  private function _sanitize($string) {
+    return filter_var(trim($string), FILTER_SANITIZE_SPECIAL_CHARS);
+  }
+
   function setSpreadsheetId($spreadsheetId) {
     $this->_spreadsheetId = $spreadsheetId;
   }
 
   function read($range) {
-    $read = $this->_service->spreadsheets_values->get($this->_spreadsheetId, $range);
-    $result = $read->getValues();
+    $result = array('success' => false);
+
+    if ($this->_spreadsheetId != '' and $range != '') {
+      try {
+        $read = $this->_service->spreadsheets_values->get($this->_spreadsheetId, $range);
+        $result = array(
+          'success' => true,
+          'result' => $read->getValues()
+        );
+      }
+      catch (Exception $e) {}
+    }
 
     return $result;
   }
